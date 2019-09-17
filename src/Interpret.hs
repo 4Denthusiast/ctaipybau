d7d44cee96aa7b8e90e9cdbed24e4dc91126b92e
4denthusiast <andrew_kr@btinternet.com>
2019-09-17 22:19:23 +0100

    Implement α-conversion
    Allow α-conversion in testing Eq Value, and substitute fresh
    variables to avoid capture when necessary.

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -9,12 +9,12 @@ import Data
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as M
 import Data.Maybe
+import Debug.Trace
 
--- TODO: Include α-conversion, and possibly other things, in this equality.
 instance Eq Value where
     (==) (VVariable l) (VVariable l')  = l == l'
-    (==) (VPi l a r) (VPi l' a' r')    = l == l' && a == a' && r == r'
-    (==) (VLambda l r) (VLambda l' r') = l == l' && r == r'
+    (==) (VPi l a r) (VPi l' a' r')    = a == a' && r == substitute l' (VVariable l) r'
+    (==) (VLambda l r) (VLambda l' r') = r == substitute l' (VVariable l) r'
     (==) (VApp f a) (VApp f' a')       = f == f' && a == a'
     (==) VBlank VBlank                 = True
     (==) (VInt i) (VInt i')            = i == i'
@@ -22,6 +22,21 @@ instance Eq Value where
     (==) TType TType                   = True
     (==) (VPrimitive l _ _ as) (VPrimitive l' _ _ as') = l == l' && as == as'
     (==) _ _                           = False
+
+isFree :: Label -> Value -> Bool
+isFree l (VVariable l') = l == l'
+isFree l (VPi l' a r) = isFree l a || (l /= l' && isFree l r)
+isFree l (VLambda l' r) = l /= l' && isFree l r
+isFree l (VApp f a) = isFree l f || isFree l a
+isFree l VBlank = False
+isFree l (VInt _) = False
+isFree l TInt = False
+isFree l TType = False
+isFree l (VPrimitive _ _ _ as) = any (isFree l) as
+
+-- Something that does not occur free in any of the values.
+newVariable :: [Value] -> Label
+newVariable vs = head $ filter (\l -> not $ any (isFree l) vs) $ map (('v':).show) [0..]
 
 -- Try to unify types. This will need some sort of context later so it can unify blanks with each other.
 unify :: Value -> Value -> Either InterpreterError ()
@@ -56,10 +71,12 @@ typecheck ctx EType = return (TType, TType)
 
 -- substitute but don't perform further reductions.
 substitute :: Label -> Value -> Value -> Value
-substitute l v = s
+substitute l v = if (v == VVariable l) then id else s
     where s (VVariable l') = if l == l' then v else (VVariable l')
           s (VPi l' a r) = if l == l' then VPi l' (s a) r else VPi l' (s a) (s r)
-          s (VLambda l' r) = if l == l' then VLambda l' r else VLambda l' (s r)
+          s (VLambda l' r) = if l == l' then VLambda l' r
+              else if (isFree l' v) then let l'' = newVariable [v,r] in VLambda l'' (s (substitute l' (VVariable l'') r))
+              else VLambda l' (s r)
           s (VApp f a) = VApp (s f) (s a)
           -- I could just use "s x = x" here, but that would be more risky if I later add more cases to Value.
           s VBlank = VBlank
@@ -68,42 +85,24 @@ substitute l v = s
           s TType = TType
           s (VPrimitive l f n as) = VPrimitive l f n (map s as)
 
--- substitute and perform any beta-reductions that result.
-substituteReduce :: Label -> Value -> Value -> Value
-substituteReduce l v = s
-    where s (VVariable l') = if l == l' then v else (VVariable l')
-          s (VPi l' a r) = if l == l' then VPi l' (s a) r else VPi l' (s a) (s r)
-          s (VLambda l' r) = if l == l' then VLambda l' r else VLambda l' (s r)
-          s (VApp f a) = headReduce (s f) (s a)
-          -- I could just use "s x = x" here, but that would be more risky if I later add more cases to Value.
-          s VBlank = VBlank
-          s (VInt i) = VInt i
-          s TInt = TInt
-          s TType = TType
-          s (VPrimitive l f n as) = applyPrimitive $ VPrimitive l f n (map s as)
-
 -- Apply beta-reduction in strict order.
 reduce :: Value -> Value
 reduce (VVariable l) = (VVariable l)
 reduce (VPi l a r) = VPi l (reduce a) (reduce r)
 reduce (VLambda l r) = VLambda l (reduce r)
-reduce (VApp f a) = headReduce (reduce f) (reduce a)
+reduce (VApp f a) = headReduce (VApp (reduce f) (reduce a))
 reduce VBlank = VBlank
 reduce (VInt i) = VInt i
 reduce TInt = TInt
 reduce TType = TType
-reduce (VPrimitive l f n as) = applyPrimitive $ VPrimitive l f n (map reduce as)
+reduce (VPrimitive l f n as) = headReduce $ VPrimitive l f n (map reduce as)
 
 -- Apply the given function to the given argument.
-headReduce :: Value -> Value -> Value
-headReduce (VLambda l r) a = substituteReduce l a r
-headReduce (VPrimitive l f n as) a | length as < n = applyPrimitive $ VPrimitive l f n (as++[a])
-headReduce f a = VApp f a -- Can't apply (e.g. f is App or Variable)
-
--- If possible, turn a VPrimitive and its collected arguments into the result value.
-applyPrimitive :: Value -> Value
-applyPrimitive p@(VPrimitive _ f _ as) = fromMaybe p (f as)
-applyPrimitive v = error "applyPrimitive is only meant to be applied to VPrimitives."
+headReduce :: Value -> Value
+headReduce (VApp (VLambda l r) a) = reduce $ substitute l a r
+headReduce (VApp (VPrimitive l f n as) a) | length as < n = headReduce $ VPrimitive l f n (as++[a])
+headReduce p@(VPrimitive _ f _ as) = fromMaybe p (f as)
+headReduce x = x -- Can't apply (e.g. (VApp f a) where f is App or Variable)
 
 evalDefinition :: Context -> (Expr, Expr) -> Either InterpreterError (Value, Value)
 evalDefinition ctx (te, ve) = do
